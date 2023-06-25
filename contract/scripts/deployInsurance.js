15283521a730d30fc0f3769b992b966cf4bb6d4f
0x2024antonio <0x2024antonio@gmail.com>
2023-06-25 01:16:42 -0400

    support polygonMumbai

--- contract/scripts/deployInsurance.js
+++ contract/scripts/deployInsurance.js
@@ -4,8 +4,20 @@ async function main() {
   const [deployer] = await hre.ethers.getSigners();
   //const owner = deployer.address;
   const contractName = "Insurance";
-  const defaultCurrency = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
-  const optimisticOracleV3 = "0x9923D42eF695B5dd9911D05Ac944d4cAca3c4EAB";
+  const networkName = process.env.HARDHAT_NETWORK;
+  const parameters = {
+    goerli: {
+      defaultCurrency: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
+      optimisticOracleV3: "0x9923D42eF695B5dd9911D05Ac944d4cAca3c4EAB",
+    },
+    polygonMumbai: {
+      defaultCurrency: "0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747",
+      optimisticOracleV3: "0x263351499f82C107e540B01F0Ca959843e22464a",
+    },
+  };
+
+  const defaultCurrency = parameters[networkName].defaultCurrency;
+  const optimisticOracleV3 = parameters[networkName].optimisticOracleV3;
 
   console.log(
     `Deploying ${contractName} contract with the account: ${deployer.address}`
