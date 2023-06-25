52ebdb412b9a728c333191ca0adcac149473001b
0x2024antonio <0x2024antonio@gmail.com>
2023-06-25 01:44:01 -0400

    added initial version of front-end

---
+++ front-end/src/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  connectorsForWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  // mainnet, polygon, optimism, arbitrum,
+  // ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  goerli,
+  polygonMumbai,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [goerli, polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: process.env.REACT_APP_WALLET_CONNECT_APP_NAME,
+  projectId: process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID,
+  chains,
+});
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      //argentWallet({ projectId, chains }),
+      //trustWallet({ projectId, chains }),
+      //ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: false,
+  connectors,
+  publicClient,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <WagmiConfig config={wagmiConfig}>
+    <RainbowKitProvider chains={chains}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </RainbowKitProvider>
+  </WagmiConfig>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
