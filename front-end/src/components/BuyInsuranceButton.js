d3927b669574dabdbf454a7b49c57b277894fa95
0x2024antonio <0x2024antonio@gmail.com>
2023-06-25 06:02:04 -0400

    require connected to wallet in buy insurance

--- front-end/src/components/BuyInsuranceButton.js
+++ front-end/src/components/BuyInsuranceButton.js
@@ -8,7 +8,11 @@ import {
   FormHelperText,
 } from "@chakra-ui/react";
 
+import { useAccount } from "wagmi";
+
 const BuyInsuranceButton = () => {
+  const { isConnected } = useAccount();
+
   const [flightNumber, setFlightNumber] = useState("");
   const [departureDate, setDepartureDate] = useState("");
   const [insuranceStatement, setInsuranceStatement] = useState("");
@@ -44,31 +48,39 @@ const BuyInsuranceButton = () => {
     <Flex flexDirection="column" gap="10">
       <Heading size="xl"> Buy flight delay insurance </Heading>
 
-      <FormControl isRequired>
-        <FormLabel>Fligt number</FormLabel>
-        <Input
-          placeholder="Flight number"
-          onChange={handleFlightNumberChange}
-        />
-      </FormControl>
+      {!isConnected && (
+        <Heading size="md"> Connect to your wallet first</Heading>
+      )}
 
-      <FormControl isRequired>
-        <FormLabel>Departure date</FormLabel>
-        <Input
-          placeholder="Departure date"
-          onChange={handleDepartureDateChange}
-        />
-      </FormControl>
+      {isConnected && (
+        <>
+          <FormControl isRequired>
+            <FormLabel>Fligt number</FormLabel>
+            <Input
+              placeholder="Flight number"
+              onChange={handleFlightNumberChange}
+            />
+          </FormControl>
 
-      <FormControl>
-        <FormLabel>Insurance statement</FormLabel>
-        <p>{`${insuranceStatement}`}</p>
-        <p disabled={true} value={insuranceStatement}></p>
-      </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Departure date</FormLabel>
+            <Input
+              placeholder="Departure date"
+              onChange={handleDepartureDateChange}
+            />
+          </FormControl>
 
-      <Button onClick={handlePreviewClick}>Preview</Button>
-      {previewed && <Button onClick={handleApproveClick}>Approve</Button>}
-      {approved && <Button onClick={handleBuyClick}>Buy</Button>}
+          <FormControl>
+            <FormLabel>Insurance statement</FormLabel>
+            <p>{`${insuranceStatement}`}</p>
+            <p disabled={true} value={insuranceStatement}></p>
+          </FormControl>
+
+          <Button onClick={handlePreviewClick}>Preview</Button>
+          {previewed && <Button onClick={handleApproveClick}>Approve</Button>}
+          {approved && <Button onClick={handleBuyClick}>Buy</Button>}
+        </>
+      )}
     </Flex>
   );
 };
